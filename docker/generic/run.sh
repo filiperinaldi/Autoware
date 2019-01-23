9c6d974933c844d2d931e7f40cb180c187e9a21c
Servando German <servando.german.serrano@linaro.org>
2019-01-23 08:44:37 +0000

    Docker: Add truly generic docker support
    Unify docker support with a new (and more generic) set of docker files.
    
    Two main images are created:
    - Base image containing an Ubuntu installation with all dependencies
    - Build image containing a pre-built Autoware
    
    When Cuda support is enabled, there is a further Base+Cuda support image.
    These Dockerfiles can be used for both AArch64 and x86_64 images.
    
    Co-authored-by: Filipe Rinaldi <filipe.rinaldi@arm.com>

--- docker/generic/run.sh
+++ docker/generic/run.sh
@@ -1,52 +1,114 @@
 #!/bin/bash
 
-usage() { echo "Usage: $0 [-t <tag>] [-r <repo>] [-s <Shared directory>]" 1>&2; exit 1; }
+# Default settings
+CUDA="on"
+IMAGE_NAME="autoware/autoware"
+TAG_PREFIX="latest"
+ROS_DISTRO="kinetic"
+BASE_ONLY="false"
 
-# Defaults
-XSOCK=/tmp/.X11-unix
-XAUTH=/home/$USER/.Xauthority
-SHARED_DIR=/home/autoware/shared_dir
-HOST_DIR=/home/$USER/shared_dir
-DOCKER_HUB_REPO="autoware/autoware"
-TAG="latest-kinetic"
+function usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "    -b,--base-only        Run the base image only."
+    echo "                          Default:$BASE_ONLY"
+    echo "    -c,--cuda <on|off>    Enable Cuda support in the Docker."
+    echo "                          Default:$CUDA"
+    echo "    -h,--help             Display the usage and exit."
+    echo "    -i,--image <name>     Set docker images name."
+    echo "                          Default:$IMAGE_NAME"
+    echo "    -t,--tag-prefix <tag> Tag prefix use for the docker images."
+    echo "                          Default:$TAG_PREFIX"
+}
 
-while getopts ":ht:r:s:" opt; do
-  case $opt in
-    h)
+OPTS=`getopt --options bc:hi:t: \
+         --long base-only,cuda:,help,image-name:,tag-prefix: \
+         --name "$0" -- "$@"`
+eval set -- "$OPTS"
+
+while true; do
+  case $1 in
+    -b|--base-only)
+      BASE_ONLY="true"
+      shift 1
+      ;;
+    -c|--cuda)
+      param=$(echo $2 | tr '[:upper:]' '[:lower:]')
+      case "${param}" in
+        "on"|"off") CUDA="${param}" ;;
+        *) echo "Invalid cuda option: $2"; exit 1 ;;
+      esac
+      shift 2
+      ;;
+    -h|--help)
       usage
-      exit
+      exit 0
       ;;
-    t)
-      TAG=$OPTARG
+    -i|--image-name)
+      IMAGE_NAME="$2"
+      shift 2
       ;;
-    r )
-      DOCKER_HUB_REPO=$OPTARG
+    -t|--tag-prefix)
+      TAG_PREFIX="$2"
+      shift 2
       ;;
-    s)
-      HOST_DIR=$OPTARG
+    --)
+      if [ ! -z $2 ];
+      then
+        echo "Invalid parameter: $2"
+        exit 1
+      fi
+      break
       ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument." >&2
+    *)
+      echo "Invalid option"
       exit 1
       ;;
   esac
 done
 
-echo "Using $DOCKER_HUB_REPO:$TAG"
-echo "Shared directory: ${HOST_DIR}"
+echo "Using options:"
+echo -e "\tROS distro: $ROS_DISTRO"
+echo -e "\tImage name: $IMAGE_NAME"
+echo -e "\tTag prefix: $TAG_PREFIX"
+echo -e "\tCuda support: $CUDA"
+echo -e "\tBase only: $BASE_ONLY"
 
-nvidia-docker run \
+SUFFIX=""
+RUNTIME=""
+
+XSOCK=/tmp/.X11-unix
+XAUTH=$HOME/.Xauthority
+
+SHARED_DOCKER_DIR=/home/autoware/shared_dir
+SHARED_HOST_DIR=$HOME/shared_dir
+
+AUTOWARE_DOCKER_DIR=/home/autoware/Autoware
+AUTOWARE_HOST_DIR=$(realpath ../..)
+
+VOLUMES="--volume=$XSOCK:$XSOCK:rw
+         --volume=$XAUTH:$XAUTH:rw
+         --volume=$SHARED_HOST_DIR:$SHARED_DOCKER_DIR:rw"
+
+if [ "$BASE_ONLY" == "true" ]; then
+    SUFFIX=$SUFFIX"-base"
+    VOLUMES="$VOLUMES --volume=$AUTOWARE_HOST_DIR:$AUTOWARE_DOCKER_DIR "
+fi
+
+if [ $CUDA == "on" ]; then
+    SUFFIX=$SUFFIX"-cuda"
+    RUNTIME="--runtime=nvidia"
+fi
+
+IMAGE=$IMAGE_NAME:$TAG_PREFIX-$ROS_DISTRO$SUFFIX
+echo "Launching $IMAGE"
+
+docker run \
     -it --rm \
-    --volume=$XSOCK:$XSOCK:rw \
-    --volume=$XAUTH:$XAUTH:rw \
-    --volume=$HOST_DIR:$SHARED_DIR:rw \
+    $VOLUMES \
     --env="XAUTHORITY=${XAUTH}" \
     --env="DISPLAY=${DISPLAY}" \
     -u autoware \
     --privileged -v /dev/bus/usb:/dev/bus/usb \
     --net=host \
-    $DOCKER_HUB_REPO:$TAG
+    $RUNTIME \
+    $IMAGE

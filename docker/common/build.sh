22f1a39fb112f0dec7b9d362d1cc47d87e385681
Servando German <servando.german.serrano@linaro.org>
2019-01-24 13:51:09 +0000

    Update

--- docker/common/build.sh
+++ docker/common/build.sh
@@ -1,16 +1,62 @@
 #!/bin/bash
 
-echo "========================================================="
-echo "Building base"
-echo "========================================================="
-docker build -t autoware:cpu_kinetic_deps -f ./docker/common/Dockerfile.cpu.kinetic_dependencies . --no-cache
+usage() { echo "Usage: $0 [-i <image name>] [--cuda <on|ON|off|OFF>]" 1>&2; exit 1; }
 
-echo "========================================================="
-echo "Building Nvidia"
-echo "========================================================="
-docker build --build-arg FROM_ARG=autoware:cpu_kinetic_deps -t autoware:nvidia_kinetic_deps -f ./docker/common/Dockerfile.nvidia.dependencies . --no-cache
+# Defaults
+CUDA="on"
+IMAGE_NAME="autoware/autoware"
+TAG_PREFFIX="local"
+ROS_DISTRO="kinetic"
 
-echo "========================================================="
-echo "Building all"
-echo "========================================================="
-docker build --build-arg FROM_ARG=autoware:nvidia_kinetic_deps -t autoware:kinetic_nvidia -f ./docker/common/Dockerfile.autoware . --no-cache
+OPTS=`getopt --options c:hi:t: --long cuda:,help,image-name:,tag-preffix: --name "$0" -- "$@"`
+eval set -- "$OPTS"
+
+while true; do
+  case $1 in
+  	-c|--cuda)
+  	  case "$2" in 
+  	  	"on"|"off"|"ON"|"OFF") CUDA="$2" ;;
+        *) echo "Invalid option: $2"; exit 1 ;;
+  	  esac
+      shift 2
+      ;;
+    -h|--help)
+      usage
+      exit
+      ;;
+    -i|--image-name)
+      IMAGE_NAME="$2"
+      shift 2
+      ;;
+	-t|--tag-preffix)
+      TAG_PREFFIX="$2"
+      shift 2
+      ;;
+    --)
+      if [ ! -z $2 ]; 
+      then
+      	echo "Invalid parameter: $2"
+      	exit 1
+ 	  fi
+      break
+      ;;
+    *)
+      echo "Invalid option"
+      exit 1
+      ;;
+  esac
+done
+
+echo "Building image $IMAGE_NAME using tag_preffix $TAG_PREFFIX, cuda $CUDA, ros $ROS_DISTRO"
+sleep 5
+
+BASE=$IMAGE_NAME:$TAG_PREFFIX-$ROS_DISTRO-base
+
+docker build -t $BASE -f Dockerfile.cpu.kinetic_dependencies ./../..
+
+if [ $CUDA = "on" ] || [ $CUDA = "ON" ] ; then
+    docker build -t $BASE-cuda --build-arg FROM_ARG=$BASE -f Dockerfile.nvidia.dependencies ./../..
+	BASE=$BASE-cuda    
+fi
+
+docker build -t $IMAGE_NAME:$TAG_PREFFIX-$ROS_DISTRO --build-arg FROM_ARG=$BASE -f Dockerfile.autoware ./../..

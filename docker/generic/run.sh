41c95b2195a7f48e1f78de801cb3a6978aa19381
sgermanserrano <servando.german.serrano@linaro.org>
2019-02-04 13:12:08 +0000

    Added pre-release option

--- docker/generic/run.sh
+++ docker/generic/run.sh
@@ -6,22 +6,25 @@ IMAGE_NAME="autoware/autoware"
 TAG_PREFIX="latest"
 ROS_DISTRO="kinetic"
 BASE_ONLY="false"
+PRE_RELEASE="off"
 
 function usage() {
     echo "Usage: $0 [OPTIONS]"
-    echo "    -b,--base-only        Run the base image only."
-    echo "                          Default:$BASE_ONLY"
-    echo "    -c,--cuda <on|off>    Enable Cuda support in the Docker."
-    echo "                          Default:$CUDA"
-    echo "    -h,--help             Display the usage and exit."
-    echo "    -i,--image <name>     Set docker images name."
-    echo "                          Default:$IMAGE_NAME"
-    echo "    -t,--tag-prefix <tag> Tag prefix use for the docker images."
-    echo "                          Default:$TAG_PREFIX"
+    echo "    -b,--base-only               Run the base image only."
+    echo "                                 Default:$BASE_ONLY"
+    echo "    -c,--cuda <on|off>           Enable Cuda support in the Docker."
+    echo "                                 Default:$CUDA"
+    echo "    -h,--help                    Display the usage and exit."
+    echo "    -i,--image <name>            Set docker images name."
+    echo "                                 Default:$IMAGE_NAME"
+    echo "    -p,--pre-release <on|off>    Use pre-release image."
+    echo "                                 Default:$PRE_RELEASE"
+    echo "    -t,--tag-prefix <tag>        Tag prefix use for the docker images."
+    echo "                                 Default:$TAG_PREFIX"
 }
 
-OPTS=`getopt --options bc:hi:t: \
-         --long base-only,cuda:,help,image-name:,tag-prefix: \
+OPTS=`getopt --options bc:hi:p:t: \
+         --long base-only,cuda:,help,image-name:,pre-release:,tag-prefix: \
          --name "$0" -- "$@"`
 eval set -- "$OPTS"
 
@@ -45,6 +48,14 @@ while true; do
       ;;
     -i|--image-name)
       IMAGE_NAME="$2"
+      shift 2
+      ;;
+    -p|--pre-release)
+      param=$(echo $2 | tr '[:upper:]' '[:lower:]')
+      case "${param}" in
+        "on"|"off") PRE_RELEASE="${param}" ;;
+        *) echo "Invalid pre-release option: $2"; exit 1 ;;
+      esac
       shift 2
       ;;
     -t|--tag-prefix)
@@ -72,6 +83,7 @@ echo -e "\tImage name: $IMAGE_NAME"
 echo -e "\tTag prefix: $TAG_PREFIX"
 echo -e "\tCuda support: $CUDA"
 echo -e "\tBase only: $BASE_ONLY"
+echo -e "\tPre-release version: $PRE_RELEASE"
 
 SUFFIX=""
 RUNTIME=""
@@ -99,9 +111,13 @@ if [ $CUDA == "on" ]; then
     RUNTIME="--runtime=nvidia"
 fi
 
+if [ $PRE_RELEASE == "on" ]; then
+    SUFFIX=$SUFFIX"-rc"
+fi
+
 IMAGE=$IMAGE_NAME:$TAG_PREFIX-$ROS_DISTRO$SUFFIX
 echo "Launching $IMAGE"
-
+sleep 5
 docker run \
     -it --rm \
     $VOLUMES \

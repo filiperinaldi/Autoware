44fb6c0260df28da72125e3ce53cc6929ef12f8c
Filipe Rinaldi <filipe.rinaldi@arm.com>
2019-02-21 14:00:46 +0000

    WIP: Docker: Add support for creating cross-arch docker images

--- docker/generic/build.sh
+++ docker/generic/build.sh
@@ -6,6 +6,8 @@ IMAGE_NAME="autoware/autoware"
 TAG_PREFIX="local"
 ROS_DISTRO="kinetic"
 BASE_ONLY="false"
+HOST_ARCH=`uname -m`
+TARGET_ARCH=`uname -m`
 
 function usage() {
     echo "Usage: $0 [OPTIONS]"
@@ -18,10 +20,12 @@ function usage() {
     echo "                          Default:$IMAGE_NAME"
     echo "    -t,--tag-prefix <tag> Tag prefix use for the docker images."
     echo "                          Default:$TAG_PREFIX"
+    echo "    --target-arch <x86_64|aarch64> Target CPU architecture. This option allows building a Docker image for a different CPU architecture other than the host's CPU architecture."
+    echo "                          Default:$TARGET_ARCH"
 }
 
 OPTS=`getopt --options bc:hi:t: \
-         --long base-only,cuda:,help,image-name:,tag-prefix: \
+         --long base-only,cuda:,help,image-name:,tag-prefix:,target-arch: \
          --name "$0" -- "$@"`
 eval set -- "$OPTS"
 
@@ -51,6 +55,10 @@ while true; do
       TAG_PREFIX="$2"
       shift 2
       ;;
+    --target-arch)
+      TARGET_ARCH=$2
+      shift 2
+      ;;
     --)
       if [ ! -z $2 ];
       then
@@ -66,18 +74,47 @@ while true; do
   esac
 done
 
+# Convert CPU architecture to Docker format
+function host_to_docker_arch() {
+    local host_arch=$1
+
+    if [ "$host_arch" == "x86_64" ]; then
+        echo "amd64"
+    elif [ "$host_arch" == "aarch64" ]; then
+        echo "arm64v8"
+    else
+        echo "Unknown arch"
+        return 1
+    fi
+    return 0
+}
+
+DOCKER_HOST_ARCH=$(host_to_docker_arch $HOST_ARCH)
+DOCKER_TARGET_ARCH=$(host_to_docker_arch $TARGET_ARCH)
+
 echo "Using options:"
 echo -e "\tROS distro: $ROS_DISTRO"
 echo -e "\tImage name: $IMAGE_NAME"
 echo -e "\tTag prefix: $TAG_PREFIX"
 echo -e "\tCuda support: $CUDA"
 echo -e "\tBase only: $BASE_ONLY"
+echo -e "\tHost architecture: $HOST_ARCH (Docker: $DOCKER_HOST_ARCH)"
+echo -e "\tTarget architecture: $TARGET_ARCH (Docker: $DOCKER_TARGET_ARCH)"
+
+if [ "$HOST_ARCH" != "$TARGET_ARCH" ]; then
+    echo "Appending $TARGET_ARCH to the image name..."
+    IMAGE_NAME=$IMAGE_NAME/$DOCKER_TARGET_ARCH
+fi
 
 BASE=$IMAGE_NAME:$TAG_PREFIX-$ROS_DISTRO-base
 
 docker build \
     --tag $BASE \
     --build-arg ROS_DISTRO=$ROS_DISTRO \
+    --build-arg HOST_ARCH=$HOST_ARCH \
+    --build-arg DOCKER_HOST_ARCH=$DOCKER_HOST_ARCH \
+    --build-arg TARGET_ARCH=$TARGET_ARCH \
+    --build-arg DOCKER_TARGET_ARCH=$DOCKER_TARGET_ARCH \
     --file Dockerfile.base ./../..
 
 CUDA_SUFFIX=""
